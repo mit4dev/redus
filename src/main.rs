946f02501a165742efda1c5382205a13fbf6cc7d
İrfan TARI <irfantari998@gmail.com>
2024-03-16 02:54:00 +0000

    support multiple pings

--- src/main.rs
+++ src/main.rs
@@ -12,18 +12,17 @@ fn main() {
                 println!("connection");
                 let mut buf = [0; 64];
 
-                match stream.read(&mut buf) {
-                    Ok(0) => {
-                        println!("Drained!");
-                    }
-                    Ok(n) => {
-                        println!("Read bytes: {}", n);
-                        stream.write_all("+PONG\r\n".as_bytes()).unwrap();
-                        stream.flush().unwrap();
-                    }
-                    Err(e) => {
-                        println!("Unknown error: {}", e);
-                        // break;
+                loop {
+                    match stream.read(&mut buf) {
+                        Ok(n) => {
+                            println!("Read bytes: {}", n);
+                            stream.write_all("+PONG\r\n".as_bytes()).unwrap();
+                            stream.flush().unwrap();
+                        }
+                        Err(e) => {
+                            println!("Unknown error: {}", e);
+                            break;
+                        }
                     }
                 }
             }
