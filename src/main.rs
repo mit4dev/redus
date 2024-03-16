0336ba8b67cb6c9ec6bcaa17240c0d38c152a62c
İrfan TARI <irfantari998@gmail.com>
2024-03-16 02:15:04 +0000

    response back with pong

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                println!("connection");
+                stream.write("+PONG\r\n".as_bytes()).unwrap();
+                stream.flush().unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
