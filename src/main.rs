1a2c4a6f03146d4315501b4a956f76f1a73e15cf
İrfan TARI <irfantari998@gmail.com>
2024-03-16 02:36:14 +0000

    handle multiple bytes at once

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+    thread,
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
                 println!("connection");
-                stream.write("+PONG\r\n".as_bytes()).unwrap();
-                stream.flush().unwrap();
+                let mut buf = [0; 64];
+
+                match stream.read(&mut buf) {
+                    Ok(n) => {
+                        println!("Read bytes: {}", n);
+                        stream.write("+PONG\r\n".as_bytes()).unwrap();
+                        stream.flush().unwrap();
+                    }
+                    Err(e) => {
+                        println!("Unknown error: {}", e);
+                        // break;
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
